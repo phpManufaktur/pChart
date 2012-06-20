85a3db29b538951dfe63410207844105444e457e
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-06-20 08:03:47 +0200

    Release 2.1.3
    moved pChart files complete to the subdirectory /pchart, added LICENSE,
    added CHANGELOG, added README.md, updated to pChart library 2.1.3,
    changed WebsiteBaker version check, updated header informations

--- info.php
+++ info.php
@@ -2,27 +2,45 @@
 
 /**
  * pChart
- * 
+ *
  * @author Jean-Damien Pogolotti
+ * @author Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @link http://www.pchart.net/
- * @copyright 2011
+ * @link http://phpmanufaktur.de
+ * @copyright 2011 - 2012
  * @license http://www.pchart.net/license
- * @version $Id$
  */
 
-// prevent this file from being accessed directly
-if (!defined('WB_PATH')) die('invalid call of '.$_SERVER['SCRIPT_NAME']);
+// include class.secure.php to protect this file and the whole CMS!
+if (defined('WB_PATH')) {
+  if (defined('LEPTON_VERSION'))
+    include(WB_PATH.'/framework/class.secure.php');
+}
+else {
+  $oneback = "../";
+  $root = $oneback;
+  $level = 1;
+  while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
+    $root .= $oneback;
+    $level += 1;
+  }
+  if (file_exists($root.'/framework/class.secure.php')) {
+    include($root.'/framework/class.secure.php');
+  }
+  else {
+    trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+  }
+}
+// end include class.secure.php
 
 $module_directory     = 'pchart';
 $module_name          = 'pChart';
-$module_function      = 'snippet';
-$module_version       = '2.1.1';
+$module_function      = (defined('LEPTON_VERSION')) ? 'library' : 'snippet';
+$module_version       = '2.1.3';
 $module_status        = 'stable';
-$module_platform      = '2.8'; 
+$module_platform      = '2.8';
 $module_author        = 'Jean-Damien Pogolotti, Ralf Hertsch (WebsiteBaker Integration)';
-$module_license       = 'GNU General Public License'; 
-$module_description   = 'pChart - a PHP Charting library'; 
+$module_license       = 'GNU General Public License';
+$module_description   = 'pChart - a PHP Charting library';
 $module_home          = 'http://www.pchart.net/';
 $module_guid          = 'D34EAD8A-BDF5-4370-80C9-E7D0C17B4EB0';
-
-?>
